999ee018c5264f73705516cf132f1b332a20b0df
Calvin Roberts <roberts.calvin85@gmail.com>
2021-03-30 13:31:39 +0100

    Add, Remove & Marked Complete.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-'use-strict';
+const form = document.getElementById('form'),
+  input = document.getElementById('input'),
+  todosUL = document.getElementById('todos');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
+
+function addTodo() {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement('li');
+    if (todo && todo.completed) {
+      todoEl.classList.add('completed');
+    }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener('click', () =>
+      todoEl.classList.toggle('completed')
+    );
+
+    todoEl.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+    });
+
+    todosUL.appendChild(todoEl);
+
+    input.value = '';
+  }
+}
